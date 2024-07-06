dd4d9adfc4e1bb9765fa6783c1b993c585a2fc9d
BryanMico <imbryanmicobaril@gmail.com>
2024-07-06 18:22:07 +0800

    Testing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 import path from "path";
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,25 +10,25 @@ import userRoutes from "./routes/user.routes.js";
 import mongoDBconn from "./db/mongoDBconn.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
-
-const __dirname = path.resolve();
-
 dotenv.config();
 
-app.use(express.json());
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
-})
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, ()=> {
     mongoDBconn();
     console.log(`Server is Running on port ${PORT}`)
 });
+
